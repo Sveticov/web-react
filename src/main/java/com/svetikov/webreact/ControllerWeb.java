345e86c265eeda0c49620af743ad97dc56a9d9de
Sveticov <sveticovdanil@gmail.com>
2020-04-09 16:49:55 +0300

    Initial commit

---
+++ src/main/java/com/svetikov/webreact/ControllerWeb.java
@@ -0,0 +1,61 @@
+package com.svetikov.webreact;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import javax.swing.*;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.springframework.http.MediaType.ALL;
+
+@Slf4j
+@RestController
+public class ControllerWeb {
+
+    List<String> program = Arrays.asList("test", " spring", " java", " react js", " angular", " react nativ");
+    List<String> program_t = new ArrayList<>();
+
+    @GetMapping("/webmono")
+    public Mono<String> web() {
+        return Mono.just("web test");
+    }
+
+    @GetMapping("/webadd/{prog}")
+    public String webadd(@PathVariable(value = "prog") String prog) {
+        program_t.add(prog);
+        return prog + " success";
+    }
+
+    @GetMapping(value = "/webflux")
+    public Flux<String> webFlux() {
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(2));
+        Flux<String> range = Flux.fromIterable(program_t).map(val -> "<div style='padding:10px; " +
+                "color:white;" +
+                " display:flex;" +
+                " margin:10px;" +
+                "background:green;'>"
+                + val +
+                "</div></br>");
+
+        return Flux.zip(range, interval).map(Tuple2::getT1);
+    }
+
+    @GetMapping("/webflux_1")
+    private Flux<String> webFlux_1() {
+        return Flux.fromIterable(program_t)
+                .map(val -> val + "</br>")
+                .delayElements(Duration.ofMillis(1000));
+    }
+
+
+}
---
+++ src/test/java/com/svetikov/webreact/WebReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svetikov.webreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
